825e74196d7a6f11333c135990c712a49d7d5f30
Finbar Ryan <finbarr@microsoft.com>
2017-09-12 16:05:17 +0100

    Initial Commit

---
+++ AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace FSharpDll.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("FSharpDll")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("FSharpDll")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("9d86a948-1c19-4137-94c4-9e2bb33e461f")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Library1.fs
@@ -0,0 +1,33 @@
+﻿namespace FSharpDll
+
+open Microsoft.Azure.WebJobs
+open System.Net
+open System.Net.Http
+open Microsoft.Azure.WebJobs.Host
+open FSharp.Data
+
+type Class1() = 
+    member this.X = "F#"
+
+
+    static member Runn(req: HttpRequestMessage, log: TraceWriter) =
+        async {
+            log.Info("start")
+
+            let result = FSharp.Data.HtmlDocument.Load("http://www.google.com")
+            log.Info("sync result")
+            log.Info(string(result.ToString().Length))
+
+            System.AppDomain.CurrentDomain.GetAssemblies()
+            |> Seq.map (sprintf "%A")
+            |> Seq.iter log.Info
+
+            try
+                let! result = HtmlDocument.AsyncLoad("http://www.google.com")
+                log.Info("async result")
+                log.Info(string (result.ToString().Length))
+            with 
+            | e -> log.Error("failed on reflection call", e)
+
+            log.Info("finished")
+        } |> Async.StartAsTask
